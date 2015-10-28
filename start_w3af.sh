18a5506bfe62099ab3773ab524ae1e6c38319054
Aleksii <alexmisnik91@gmail.com>
2015-10-28 12:40:14 +0300

    Update start_w3af.sh

--- start_w3af.sh
+++ start_w3af.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-touch   /home/mewded/Ark/w3af/all_vuln1.w3af | chmod 755 /home/mewded/Ark/w3af/all_vuln1.w3af;
-cat /home/mewded/Ark/w3af/W3AF/all_vuln.w3af  | sed 's/dom/$1/g'  >>  /home/mewded/Ark/w3af/all_vuln1.w3af;
-chmod u+x all_vuln1.w3af;
+echo "Копирование исходного скрипта запуска";
+touch /home/"$USER"/Ark/w3af/W3AF/all_vuln1.w3af | chmod 755 /home/"$USER"/Ark/w3af/W3AF/all_vuln1.w3af;
+echo "Замена параметра имени";
+cat /home/"$USER"/Ark/w3af/W3AF/all_vuln.w3af  | sed 's/dom/'$1'/g'  >>  /home/"$USER"/Ark/w3af/all_vuln1.w3af;
+echo "Права на исполнение ";
+chmod 755 all_vuln1.w3af;
+echo "Запуск скрипта сканирования";
+cd  /home/"$USER"/Ark/w3af/;
 ./w3af_console -s all_vuln1.w3af;
+echo "Удаление созданого скрипта";
 rm all_vuln1.w3af;
