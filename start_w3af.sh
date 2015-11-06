16447b17777d3082a217f2b92d26687ee45b9604
Aleksii <alexmisnik91@gmail.com>
2015-11-06 12:34:17 +0300

    Update start_w3af.sh

--- start_w3af.sh
+++ start_w3af.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-echo "Копирование исходного скрипта запуска";
-touch /home/"$USER"/Ark/w3af/W3AF/all_vuln1.w3af | chmod 755 /home/"$USER"/Ark/w3af/W3AF/all_vuln1.w3af;
-echo "Замена параметра имени";
-cat /home/"$USER"/Ark/w3af/W3AF/all_vuln.w3af  | sed 's/dom/'$1'/g'  >>  /home/"$USER"/Ark/w3af/all_vuln1.w3af;
-echo "Права на исполнение ";
-chmod 755 all_vuln1.w3af;
-echo "Запуск скрипта сканирования";
-cd  /home/"$USER"/Ark/w3af/;
-./w3af_console -s all_vuln1.w3af;
-echo "Удаление созданого скрипта";
-rm all_vuln1.w3af;
+
+cd /home/aleksii/w3af/W3AF
+touch  Script.w3af | chmod 755 Script.w3af
+sed 's|set|set target http://cert.gov.ua|' MyScript.w3af > Script.w3af 
+./w3af_console -s Script.w3af 
